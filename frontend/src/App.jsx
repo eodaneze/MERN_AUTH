08a91d2515c2e9417dc8ddff937710ccf44cd8af
eodaneze <ezealiokechukwu1999@gmail.com>
2023-09-16 15:20:18 -0700

    setting up react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom';
 import Header from './components/Header'
-import HomeScreen from './screens/HomeScreen'
+import { Container } from 'react-bootstrap';
 
 const App = () => {
   return (
     <div>
       
       <Header />
-      <HomeScreen />
+      <Container className='my-2'>
+        <Outlet />
+      </Container>
     </div>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+
+import HomeScreen from './screens/HomeScreen';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
