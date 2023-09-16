60e4fac675bb60efd3b4b2d0d1f2173ed58f88f2
eodaneze <ezealiokechukwu1999@gmail.com>
2023-09-16 15:00:33 -0700

    designing the header components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <div>
-      <h1>mern app</h1>
+      
+      <Header />
     </div>
   )
 }
