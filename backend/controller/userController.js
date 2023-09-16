37896aaaad874403a868b9b7594e9724814d9506
eodaneze <ezealiokechukwu1999@gmail.com>
2023-09-16 12:48:55 -0700

    user login

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -6,8 +6,22 @@ import generateToken from "../utils/generateToken.js";
 // @access public
 
 // the asynchandler helps us to use async await without having to use try and catch
+// user login
 const authUser = asyncHandler(async(req, res) => {
-  res.status(200).json({message: "Auth user"});
+ const {email, password} = req.body;
+ const user = await User.findOne({email});
+ if(user){
+  generateToken(res, user._id)
+  res.status(201).json({
+    message: "Login successful",
+    _id: user._id,
+    name: user.name,
+    email: user.email
+  })
+}else{
+  res.status(401);
+  throw new Error('Invalid email or password')
+}
 });
 
 // @desc register a new user
