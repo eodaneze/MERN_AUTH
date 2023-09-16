29199344a5ee284dd87d76534c0ad538535a2146
eodaneze <ezealiokechukwu1999@gmail.com>
2023-09-16 15:21:58 -0700

    using a link container from react-boostrap to aviod page refresh on navigation

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
 
 const Header = () => {
@@ -6,16 +7,22 @@ const Header = () => {
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>MERN Auth</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>MERN App</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <Nav.Link href='/login'>
-                <FaSignInAlt /> Sign In
-              </Nav.Link>
-              <Nav.Link href='/register'>
-                <FaSignOutAlt /> Sign Up
-              </Nav.Link>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <FaSignInAlt /> Sign In
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/register'>
+                <Nav.Link>
+                  <FaSignOutAlt /> Sign Up
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
