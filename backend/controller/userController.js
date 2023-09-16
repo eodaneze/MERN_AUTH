6beeda87c70b1b81c0f87b82316ed3313e45821b
eodaneze <ezealiokechukwu1999@gmail.com>
2023-09-16 11:19:32 -0700

    creating different user routes

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -5,11 +5,40 @@ import asyncHandler from "express-async-handler";
 
 // the asynchandler helps us to use async await without having to use try and catch
 const authUser = asyncHandler(async(req, res) => {
-    res.status(401);
-    throw new Error('Something went wrong')
   res.status(200).json({message: "Auth user"});
-})
+});
+
+// @desc register a new user
+// route POST/api/users
+// @access public
+const registerUser = asyncHandler(async(req, res) => {
+  res.status(200).json({message: "Register User"});
+});
+// @desc logout user
+// route POST/api/users/logout
+// @access public
+const logoutUser = asyncHandler(async(req, res) => {
+  res.status(200).json({message: "logout User"});
+});
+// @desc GET user profile
+// route POST/api/users/profile
+// @access private
+const getUserProfile = asyncHandler(async(req, res) => {
+  res.status(200).json({message: "user profile"});
+});
+
+// @desc upadte user profile
+// route PUT/api/users/profile
+// @access private
+const updateUserProfile = asyncHandler(async(req, res) => {
+  res.status(200).json({message: "update user profile"});
+});
+
 
 export {
-    authUser
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,16 @@
 import express from 'express';
 const router = express.Router();
-import { authUser } from '../controller/userController.js';
+import { 
+    authUser,  
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile 
+} from '../controller/userController.js';
 
-router.post('/auth', authUser)
+router.post('/', registerUser);
+router.post('/auth', authUser);
+router.post('/logout', logoutUser);
+router.route('/profile').get(getUserProfile).put(updateUserProfile)
 
 export default router
