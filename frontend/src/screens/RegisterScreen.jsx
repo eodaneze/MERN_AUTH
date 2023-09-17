aab6b5c360331d6ba9c69db9f86e9b0c930f7033
eodaneze <ezealiokechukwu1999@gmail.com>
2023-09-17 12:01:33 -0700

    implementing the user registration from the userApiSlice

--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -1,7 +1,12 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { Form, Button, Row, Col } from 'react-bootstrap';
+import {useDispatch, useSelector} from 'react-redux'
 import FormContainer from '../components/FormContainer';
+import { toast } from 'react-toastify';
+import Loader from '../components/Loader';
+import { useRegisterMutation } from '../slices/userApiSlice';
+import { setCredentials } from '../slices/authSlice';
 
 const RegisterScreen = () => {
   const [name, setName] = useState('');
@@ -9,9 +14,35 @@ const RegisterScreen = () => {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
 
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  
+  const {userInfo} = useSelector((state) => state.auth);
+
+  
+  const [register, {isLoading}] = useRegisterMutation();
+
+  
+  useEffect(() => {
+    if(userInfo){
+     navigate('/')
+    }
+ }, [navigate, userInfo])
+
   const submitHandler = async (e) => {
     e.preventDefault();
-    console.log('submit');
+    if(password !== confirmPassword){
+       toast.error('Password do not match')
+    }else{
+       try {
+        const res = await register({name, email, password}).unwrap();
+        dispatch(setCredentials({...res}))
+        toast.success('Your registration was successful! Login now');
+        navigate('/login')
+       } catch (err) {
+        toast.error(err?.data?.message || err.error)
+       }
+    }
   };
 
   return (
@@ -56,7 +87,7 @@ const RegisterScreen = () => {
             onChange={(e) => setConfirmPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
-
+        {isLoading && <Loader />}
         <Button type='submit' variant='primary' className='mt-3'>
           Register
         </Button>
