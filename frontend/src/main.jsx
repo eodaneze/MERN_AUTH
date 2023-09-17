01b9289a8a7bd35ed0401f3b177f37727ba44d19
eodaneze <ezealiokechukwu1999@gmail.com>
2023-09-17 08:14:23 -0700

    setting up the store using redux toolkit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,6 +11,8 @@ import {
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen.jsx';
 import RegisterScreen from './screens/RegisterScreen.jsx';
+import store from './store.js';
+import { Provider } from 'react-redux';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -23,7 +25,11 @@ const router = createBrowserRouter(
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
+
+  </Provider>
 );
