778b220d86bda28fea0ec082e1eadfebd97b4d0f
eodaneze <ezealiokechukwu1999@gmail.com>
2023-09-17 09:10:05 -0700

    creating a userApiSlice

---
+++ frontend/src/slices/userApiSlice.js
@@ -0,0 +1,17 @@
+import { apiSlice } from "./apiSlice";
+
+const USERS_URL = '/api/users';
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+         login: builder.mutation({
+            query: (data) => ({
+                url: `${USERS_URL}/auth`,
+                method: 'POST',
+                body: data
+            })
+         })
+    })
+})
+
+export const {useLoginMutation} = usersApiSlice
