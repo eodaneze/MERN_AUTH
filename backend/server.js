d9ff4ea0651624e8d0b8a56acecc8d6a9e3d757f
eodaneze <ezealiokechukwu1999@gmail.com>
2023-09-18 00:49:07 -0700

    setting up the frontend and backend for production

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
@@ -14,11 +15,20 @@ app.use(express.json())
 // this we allow form data to be sent
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
-app.use('/api/users', userRoutes)
-app.get('/', (req, res) => {
-     res.send("server is ready");
-     console.log("HELLO FROM NODE JS");
-})
+app.use('/api/users', userRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+     const __dirname = path.resolve();
+     app.use(express.static(path.join(__dirname, '/frontend/dist')));
+   
+     app.get('*', (req, res) =>
+       res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+     );
+   } else {
+     app.get('/', (req, res) => {
+       res.send('API is running....');
+     });
+   }
 
 app.use(notFound);
 app.use(errorHandler);
